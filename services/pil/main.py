b6f4cddf23d70c7ae718fbc76858222258154f3c
Moin Sarwar <moinsarwar19@gmail.com>
2026-07-07 19:15:22 +0500

    Fix postgres port parsing

--- services/pil/main.py
+++ services/pil/main.py
@@ -27,7 +27,7 @@ except Exception as e:
 def get_db_connection():
     return psycopg2.connect(
         host=os.getenv('POSTGRES_HOST', 'postgres'),
-        port=os.getenv('POSTGRES_PORT', '5432'),
+        port=5432,
         database=os.getenv('POSTGRES_DB', 'financial_system'),
         user=os.getenv('POSTGRES_USER', 'postgres'),
         password=os.getenv('POSTGRES_PASSWORD', 'postgres')
